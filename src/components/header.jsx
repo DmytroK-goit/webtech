c382f3b77c14ce781b3ef828c8eb42510bcaca70
Dmytro Kovbasiuk <159040488+DmytroK-goit@users.noreply.github.com>
2026-02-25 12:55:47 +0200

    add header more

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,20 +1,30 @@
 import Image from "next/image";
 import Logo from "../img/header/logo.png";
 import Link from "next/link";
+
 export default function Header() {
   return (
-    <header className="py-6">
-      <div className="container mx-auto flex items-center gap-4">
-        <Image src={Logo} alt="WebTech logo" width={51} height={30} />
+    <header className="bg-[#171717] py-6">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="flex items-center gap-4">
+          <Image src={Logo} alt="WebTech logo" width={51} height={30} />
+          <Link
+            href="/"
+            className="text-[30px] font-semibold text-[#377dff] font-mono"
+          >
+            WebTech
+          </Link>
+        </div>
 
-        <Link
-          href="/"
-          className="text-[30px] font-semibold text-[#377dff] font-mono"
-        >
-          WebTech
-        </Link>
+        <nav className="hidden md:flex gap-8 text-white font-medium">
+          <Link href="/">Home</Link>
+          <Link href="/">Pages</Link>
+          <Link href="/">Services</Link>
+          <Link href="/">Portfolio</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">Contact Us</Link>
+        </nav>
       </div>
-      Header
     </header>
   );
 }
