ced2bd2194866d3204fa1edc6c0da9451bd4fef3
Dmytro Kovbasiuk <159040488+DmytroK-goit@users.noreply.github.com>
2026-02-28 13:31:05 +0200

    upd reasons

--- src/components/reasons.jsx
+++ src/components/reasons.jsx
@@ -1,11 +1,41 @@
 import s from "../scss/reasons.module.scss";
 export default function Reasons() {
   return (
-    <section className={`${s.reasons_section} w-full h-310`}>
-      <div>
-        {" "}
-        <h2>Few Reasons</h2>
-        <h2>Why Choose Us</h2>
+    <section className={`${s.reasons_section} w-full h-310 pt-37 pl-30`}>
+      <div className=" flex flex-col w-1/3 border-l-2 border-l-blue-600 pl-3 ">
+        <h2 className="text-3xl text-black">Few Reasons</h2>
+        <h2 className="text-3xl text-blue-800">Why Choose Us</h2>
+      </div>
+      <div className="pt-30 flex flex-col gap-6 max-w-1/3">
+        <div className="flex flex-col gap-5">
+          <h3 className="text-4xl font-bold text-blue-600">.01</h3>
+          <p className="text-xl font-semibold mt-4 text-[#233142]">Clients</p>
+          <p className="mt-3 text-gray-600 leading-relaxed">
+            We build strong, long-term partnerships with our clients by
+            understanding their goals and delivering tailored digital solutions
+            that drive real business growth and brand recognition.
+          </p>
+        </div>
+        <div className="flex flex-col gap-5">
+          <h3 className="text-4xl font-bold text-blue-600">.02</h3>
+          <p className="text-xl font-semibold mt-4  text-[#233142]">Projects</p>
+          <p className="mt-3 text-gray-600 leading-relaxed">
+            Every project we create is focused on usability, performance, and
+            modern design. From concept to launch, we ensure each product meets
+            high quality standards and user expectations.
+          </p>
+        </div>
+        <div className="flex flex-col gap-5">
+          <h3 className="text-4xl font-bold text-blue-600">.03</h3>
+          <p className="text-xl font-semibold mt-4  text-[#233142]">
+            Reporting & Analysis
+          </p>
+          <p className="mt-3 text-gray-600 leading-relaxed">
+            We continuously analyze results and user behavior to optimize
+            performance. Clear reporting and data-driven insights help improve
+            strategies and maximize return on investment.
+          </p>
+        </div>
       </div>
     </section>
   );
